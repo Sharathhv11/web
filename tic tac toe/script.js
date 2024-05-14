512f6f0dd3dffe3596c20f952b96ff19022885c8
Sharath <132804379+Sharathhv11@users.noreply.github.com>
2024-05-14 07:55:35 +0530

    Add files via upload
    tic tac toe game update commit

---
+++ tic tac toe/script.js
@@ -0,0 +1,43 @@
+
+let playersName=0;
+function form() {
+  const input = document.querySelector("input");
+  const button = document.querySelector("button");
+  
+
+  button.addEventListener("click", (e) => {
+    e.stopPropagation();
+    
+    if (playersName !== 2) {
+      if (input.value.trim() === "") {
+        document.querySelector("h1").innerHTML = "please enter valid name";
+      } else {
+        const value = input.value.trim();
+        sessionStorage.setItem(input.previousElementSibling.innerHTML,value);
+        input.previousElementSibling.innerHTML = "player 2";
+        input.value = "";
+        playersName++
+        
+        console.log(playersName)
+        if (playersName === 2) {
+          input.previousElementSibling.innerHTML = "play";
+          input.remove();
+          button.innerHTML = "<a href='./play.html'>play</a>";
+         
+        }
+      }
+    }
+  });
+  const inputDiv = document.getElementById("input");
+  inputDiv.addEventListener("click", () => {
+    inputDiv.lastElementChild.innerHTML = "";
+    
+  });
+}
+
+async function main() {
+  form();
+  
+}
+
+main();
